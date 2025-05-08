23785df717ad89c151c4bc4db01565ac20a8a21c
Zephrynis <210824152+harry-w-dev@users.noreply.github.com>
2025-05-08 09:47:00 +0100

    Added database connection

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,14 @@
+import { error } from "@sveltejs/kit";
+import type { LayoutServerLoad } from "./$types";
+import { connect_to_db } from "$lib/server/db";
+
+export const load: LayoutServerLoad = async (event) => {
+    const connection = await connect_to_db();
+    if (!connection) {
+        throw error(500, "Failed to connect to the database");
+    }
+    const name = event.cookies.get("name") ?? "";
+    const email = event.cookies.get("email") ?? "";
+
+    return { name, email };
+}
