31e439107b73975e19e1b579aa5f444dbd2bf59f
Zephrynis <210824152+harry-w-dev@users.noreply.github.com>
2025-05-10 00:49:01 +0100

    Make root page redirect to correct page

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,12 @@
+import { authenticate } from "$lib/server/authenticate";
+import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async (event) => {
+    const auth = authenticate(event.cookies);
+    if (auth) {
+        throw redirect(307, "/dashboard");
+    } else {
+        throw redirect(307, "/auth/login");
+    }
+}
