78a58afadcf87665dccfc4ab5b72cd850b6a97b0
Zephrynis <210824152+harry-w-dev@users.noreply.github.com>
2025-05-10 00:51:18 +0100

    Various login changes
    - Fade in/out on enter/exit
    - edit register redirect button
    - probably other stuff, forgot exactly what

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -2,7 +2,21 @@
 	import { enhance } from "$app/forms";
 	import type { ActionData } from "./$types";
 
-	export let form: ActionData;
+	let transitionPage = $state(true);
+	
+	const { form }: { form: ActionData } = $props();
+
+	function registerPage() {
+		transitionPage = true;
+		setTimeout(() => {
+			window.location.href = "/auth/register";
+		}, 200);
+	}
+
+	setTimeout(() => {
+		transitionPage = false;
+	}, 200);
+
 </script>
 
 <svelte:head>
@@ -10,36 +24,35 @@
 </svelte:head>
 
 <div class="flex w-full h-screen sm:items-center justify-center bg-zinc-900 text-zinc-300">
-<div class="w-full p-5 min-[540px]:max-w-96 flex flex-col gap-8">
-	<div class="flex flex-col items-center">
-		<p class="text-3xl font-semibold">Login</p>
-		<p>Please enter your credentials to login.</p>
+	<div class="w-full p-5 min-[540px]:max-w-96 flex flex-col gap-8 {transitionPage ? 'opacity-0' : 'opacity-100'} transition-opacity duration-200 ease-in-out">
+		<div class="flex flex-col items-center">
+			<p class="text-3xl font-semibold">Login</p>
+			<p>Please enter your credentials to login.</p>
+		</div>
+
+		<form class="flex flex-col gap-6" method="POST" autocomplete="off" use:enhance>
+			<div class="flex flex-col items-start gap-1">
+				<label for="email_input">Email</label>
+				<input class="w-full" type="email" id="email_input" name="email" placeholder="email@example.com" value={form?.email ?? ""} />
+			</div>
+			<div class="flex flex-col items-start gap-1">
+				<label for="password_input">Password</label>
+				<input class="w-full" type="password" id="password_input" name="password" placeholder="SuperStrongPassword" />
+			</div>
+			<button class="w-full bg-zinc-700 p-4 mt-6 rounded-lg hover:bg-zinc-600 transition ease-in-out duration-200">Login</button>
+		</form>
+		<div class="flex gap-1 justify-center">
+			<p class="">Don't have an account?</p>
+			<button type="button" class="text-zinc-200 hover:text-zinc-50 transition ease-in-out group cursor-pointer" onclick={registerPage} aria-label="Register">
+				Register
+				<div class="bg-zinc-200 h-[2px] w-0 group-hover:w-full transition-all duration-500 cursor-pointer"></div>
+			</button>
+		</div>
+
+		{#if form?.error}
+			<p class="error">
+				{form.error}
+			</p>
+		{/if}
 	</div>
-
-	<form class="flex flex-col gap-6" method="POST" autocomplete="off" use:enhance>
-		<div class="flex flex-col items-start gap-1">
-			<label for="email_input">Email</label>
-			<input class="w-full" type="email" id="email_input" name="email" placeholder="email@example.com" value={form?.email ?? ""} />
-		</div>
-		<div class="flex flex-col items-start gap-1">
-			<label for="password_input">Password</label>
-			<input class="w-full" type="password" id="password_input" name="password" placeholder="SuperStrongPassword" />
-		</div>
-		<button class="w-full bg-zinc-700 p-4 mt-6 rounded-lg hover:bg-zinc-600 transition ease-in-out duration-200">Login</button>
-	</form>
-
-	<p class="text-center">Don't have an account? <a href="/auth/register">Register</a></p>
-
-	{#if form?.user}
-		<p class="success">
-			Welcome {form.user.name}! You can now open the dashboard.
-		</p>
-	{/if}
-
-	{#if form?.error}
-		<p class="error">
-			{form.error}
-		</p>
-	{/if}
-</div>
 </div>
