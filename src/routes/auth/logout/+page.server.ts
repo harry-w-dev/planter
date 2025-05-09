545b37fd7e3f26cf4ddde83c2d02558528ad54a7
Zephrynis <210824152+harry-w-dev@users.noreply.github.com>
2025-05-10 00:49:57 +0100

    Change cookie options to match setting

--- src/routes/auth/logout/+page.server.ts
+++ src/routes/auth/logout/+page.server.ts
@@ -1,9 +1,10 @@
 import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
+import { cookie_options } from "$lib/server/utils";
 
 export const load: PageServerLoad = async (event) => {
-    event.cookies.delete("auth-token", { path: "/" });
-    event.cookies.delete("email", { path: "/" });
-    event.cookies.delete("name", { path: "/" });
+    event.cookies.delete("auth-token", cookie_options);
+    event.cookies.delete("email", cookie_options);
+    event.cookies.delete("name", cookie_options);
     throw redirect(301, "/");
 };
