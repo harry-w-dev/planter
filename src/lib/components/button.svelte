76e8b8a3f9355803e054bb6d1191ce4527f14484
harry-w-dev <210824152+harry-w-dev@users.noreply.github.com>
2025-05-11 01:05:08 +0100

    Created button & input components

---
+++ src/lib/components/button.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+    let { color, children, class: className } = $props();
+</script>
+
+<style>
+    .button {
+        padding: 1rem;
+        margin-top: 1.5rem;
+        border-radius: 0.5rem;
+        transition: background-color 0.2s ease-in-out;
+        cursor: pointer;
+    }
+
+    .button-primary {
+        background-color: var(--color-primary);
+        color: var(--color-primary-content)
+    }
+
+    .button-secondary {
+        background-color: var(--color-secondary);
+        color: var(--color-secondary-content);
+    }
+
+    .button-accent {
+        background-color: var(--color-accent);
+        color: var(--color-accent-content);
+    }
+
+    .button-neutral {
+        background-color: var(--color-neutral);
+        color: var(--color-neutral-content);
+    }
+
+    .button-info {
+        background-color: var(--color-info);
+        color: var(--color-info-content);
+    }
+
+    .button-success {
+        background-color: var(--color-success);
+        color: var(--color-success-content);
+    }
+
+    .button-warning {
+        background-color: var(--color-warning);
+        color: var(--color-warning-content);
+    }
+
+    .button-error {
+        background-color: var(--color-error);
+        color: var(--color-error-content);
+    }
+
+    .button-primary:hover {
+        background-color: color-mix(in oklab, var(--color-primary, var(--color-base-200)), #000 7%);
+    }
+
+    .button-secondary:hover {
+        background-color: color-mix(in oklab, var(--color-secondary, var(--color-base-200)), #000 7%);
+    }
+
+    .button-accent:hover {
+        background-color: color-mix(in oklab, var(--color-accent, var(--color-base-200)), #000 7%);
+    }
+
+    .button-neutral:hover {
+        background-color: color-mix(in oklab, var(--color-neutral, var(--color-base-200)), #000 7%);
+    }
+
+    .button-info:hover {
+        background-color: color-mix(in oklab, var(--color-info, var(--color-base-200)), #000 7%);
+    }
+
+    .button-success:hover {
+        background-color: color-mix(in oklab, var(--color-success, var(--color-base-200)), #000 7%);
+    }
+
+    .button-warning:hover {
+        background-color: color-mix(in oklab, var(--color-warning, var(--color-base-200)), #000 7%);
+    }
+
+    .button-error:hover {
+        background-color: color-mix(in oklab, var(--color-error, var(--color-base-200)), #000 7%);
+    }
+</style>
+
+<button class="button {color ? `button-${color}` : 'button-primary'} {className}">
+    {#if children}
+        {@render children()}
+    {:else}
+        {#if color === "primary"}
+            Primary Button
+        {:else if color === "secondary"}
+            Secondary Button
+        {:else}
+            Default Button
+        {/if}
+    {/if}
+</button>
---
+++ src/lib/components/input.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    let { 
+        placeholder = null,
+        oninput = null, 
+        children = null, 
+        class: className = null, 
+        error = null, 
+        value = null, 
+        type = null, 
+        id = null, 
+        name = null, 
+        label = null
+    } = $props();
+</script>
+
+<style>
+    .input {
+        background-color: var(--color-base-100);
+        outline: 1px solid color-mix(in oklab,var(--color-base-content)20%,#0000);
+        padding: calc(var(--spacing) * 2);
+        border-radius: var(--radius-lg);
+        transition-timing-function: var(--tw-ease, var(--default-transition-timing-function));
+        transition-duration: 100ms;
+        transition-property: background-color, border-color, box-shadow, transform;
+    }
+
+    .input-error {
+        outline: 2px solid var(--color-error);
+        outline-offset: 1px;
+    }
+
+    .input:focus {
+        outline: 2px solid var(--color-base-content);
+    }
+</style>
+
+<div class="flex flex-col items-start gap-1">
+    {#if label}
+    <div class="flex flex-row justify-between w-full">
+        <label for={id}>{label}</label>
+        <span class="text-error">{error ? error : ""}</span>
+    </div>
+    {/if}
+    <input
+        oninput={oninput}
+        class="input {className} {error ? "input-error" : ""}"
+        type={type}
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+    />
+</div>
