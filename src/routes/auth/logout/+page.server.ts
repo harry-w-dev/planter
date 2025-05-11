07d316d28f8be3fd2eeeaf56502b97704fc244ae
harry-w-dev <210824152+harry-w-dev@users.noreply.github.com>
2025-05-11 01:13:37 +0100

    Change logout to be called by forms

--- src/routes/auth/logout/+page.server.ts
+++ src/routes/auth/logout/+page.server.ts
@@ -1,10 +1,12 @@
 import { redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
+import type { Actions } from "./$types";
 import { cookie_options } from "$lib/server/utils";
 
-export const load: PageServerLoad = async (event) => {
-    event.cookies.delete("auth-token", cookie_options);
-    event.cookies.delete("email", cookie_options);
-    event.cookies.delete("name", cookie_options);
-    throw redirect(301, "/");
+export const actions: Actions = {
+    default: async (event) => {
+        event.cookies.delete("auth-token", cookie_options);
+        event.cookies.delete("email", cookie_options);
+        event.cookies.delete("name", cookie_options);
+        throw redirect(301, "/");
+    }
 };
