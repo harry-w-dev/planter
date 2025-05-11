ffa2449417dd2d137b3ad9003f55acf70a306be1
harry-w-dev <210824152+harry-w-dev@users.noreply.github.com>
2025-05-11 01:05:42 +0100

    Make login page use components

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -2,16 +2,24 @@
 	import { enhance } from "$app/forms";
 	import { goto } from "$app/navigation";
 	import type { ActionData } from "./$types";
+	import Button from "$lib/components/button.svelte";
+	import Input from "$lib/components/input.svelte";
 
 	let transitionPage = $state(true);
 	
-	const { form }: { form: ActionData } = $props();
+	let { form }: { form: ActionData } = $props();
 
 	function registerPage() {
 		transitionPage = true;
 		setTimeout(() => {
 			goto("/auth/register", { replaceState: true });
 		}, 200);
+	}
+
+	function removeError(option: string) {
+		if (form?.error === option) {
+			form = { ...form, error: "" };
+		}
 	}
 
 	setTimeout(() => {
@@ -24,29 +32,36 @@
 	<title>Login</title>
 </svelte:head>
 
-<div class="flex w-full h-screen sm:items-center justify-center bg-zinc-900 text-zinc-300">
-	<div class="w-full p-5 min-[540px]:max-w-96 flex flex-col gap-8 {transitionPage ? 'opacity-0' : 'opacity-100'} transition-opacity duration-200 ease-in-out">
+<div class="flex w-full h-screen sm:items-center justify-center bg-base-200 text-base-content">
+	<div class="bg-base-100 outline-1 outline-base-300 rounded-lg w-full p-5 min-[540px]:max-w-96 flex flex-col gap-8 {transitionPage ? 'opacity-0' : 'opacity-100'} transition-opacity duration-200 ease-in-out">
 		<div class="flex flex-col items-center">
 			<p class="text-3xl font-semibold">Login</p>
 			<p>Please enter your credentials to login.</p>
 		</div>
 
 		<form novalidate class="flex flex-col gap-6" method="POST" autocomplete="off" use:enhance>
-			<div class="flex flex-col items-start gap-1">
-				<div class="flex flex-row justify-between w-full">
-					<label for="password_input">Email</label>
-					<span class="text-red-400">{form?.error === "email" ? form.message : ""}</span>
-				</div>
-				<input class="w-full {form?.error === "email" ? "outline-2 outline-offset-1 outline-red-400" : ""}" type="email" id="email_input" name="email" placeholder="email@example.com" value={form?.email ?? ""} />
-			</div>
-			<div class="flex flex-col items-start gap-1">
-				<div class="flex flex-row justify-between w-full">
-					<label for="password_input">Password</label>
-					<span class="text-red-400">{form?.error === "password" ? form.message : ""}</span>
-				</div>
-				<input class="w-full {form?.error === "password" ? "outline-2 outline-offset-2 outline-red-400" : ""}" type="password" id="password_input" name="password" placeholder="SuperStrongPassword" />
-			</div>
-			<button class="w-full bg-zinc-700 p-4 mt-6 rounded-lg hover:bg-zinc-600 transition ease-in-out duration-200">Login</button>
+			<Input 
+				label="Email" 
+				error={form?.error === "email" ? form?.message : undefined} 
+				oninput={() => removeError("email")} 
+				class="w-full" 
+				type="email" 
+				id="email_input" 
+				name="email" 
+				placeholder="email@example.com" 
+				value={form?.email ?? ""} 
+			/>
+			<Input 
+				label="Password"
+				error={form?.error === "password" ? form?.message : undefined} 
+				oninput={() => removeError("password")} 
+				class="w-full" 
+				type="password" 
+				id="password_input" 
+				name="password" 
+				placeholder="SuperStrongPassword" 
+			/>
+			<Button color="primary" class="w-full">Login</Button>
 		</form>
 		<div class="flex gap-1 justify-center">
 			<p class="">Don't have an account?</p>
