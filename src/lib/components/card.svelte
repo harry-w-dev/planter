d5b2efca7423ada8a7983875f5f63508bdd44d2a
harry-w-dev <210824152+harry-w-dev@users.noreply.github.com>
2025-05-12 11:36:32 +0100

    Created card component

---
+++ src/lib/components/card.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let { children = null, class: className = "" } = $props();
+</script>
+
+<div class="p-4 bg-base-100 text-base-content rounded-lg {className}">
+    {#if children}
+        {@render children()}
+    {:else}
+        <p>Hello world</p>
+    {/if}
+</div>
