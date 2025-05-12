5fc61d0edb55b799fe51cbedbf26c3e1c7fdc6a3
harry-w-dev <210824152+harry-w-dev@users.noreply.github.com>
2025-05-12 11:36:19 +0100

    Add project to schema

--- src/lib/server/database/model.ts
+++ src/lib/server/database/model.ts
@@ -8,7 +8,13 @@ const User_Schema = new mongoose.Schema({
 
 const Todo_Schema = new mongoose.Schema({
 	id: { type: String, required: true, unique: true },
-	name: { type: String, required: true }
+	name: { type: String, required: true },
+	users: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		}
+	]
 });
 
 export const User_Model = mongoose.model("User", User_Schema);
